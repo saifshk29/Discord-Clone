803111554188cc9067d2cef11ec41c2ec670de49
saifshk29 <saifshk29@gmail.com>
2024-02-17 00:15:49 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily:{
+    Whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
+}
+
